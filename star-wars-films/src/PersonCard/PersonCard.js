ad545ab6e5eec6ee75f089093343306fb9b56ed8
Watkins <watkins@sea-watkins-m3.ds.ad.adp.com>
2018-05-20 19:38:09 -0700

    Got the photos in the design.

--- star-wars-films/src/PersonCard/PersonCard.js
+++ star-wars-films/src/PersonCard/PersonCard.js
@@ -10,12 +10,14 @@ class PersonCard extends Component {
     const people = this.props.people;
     const num = parseInt(this.props.indexNum);
     const indexNumber = num + 1;
+    const backgroundMediaStyles = {
+        backgroundImage: 'url(\'/characters/character-' + indexNumber + '.jpg\')',
+    };
     return (
 
       <div className="person-card">
       <Link to={`/people/${indexNumber}`} style={{ textDecoration: 'none' }}>
-        <div className="media"></div>
-
+        <div className="media" style={backgroundMediaStyles}></div>
         <div className="container">
           <p className="styleized-font"><span>{people.name}</span></p>
           <h1>{people.name}</h1>
