bcfdf43e47ba80bda9a37a2cdf5d91b53e113549
sheyna <sheynawatkins@gmail.com>
2023-10-02 19:53:43 -0700

    Updated to functional components.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,72 +1,58 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import PersonCard from '../PersonCard/PersonCard';
 import './Home.css';
 
-import PropTypes from 'prop-types';
+const initialPage = 1;
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      people: [],
-      page: this.props.page
+function Home() {
+  const [ page, setPage ] = useState(initialPage);
+  const [ people, setPeople ] = useState([]);
+
+  useEffect(function() {
+    async function getPeopleInfo() {
+      let url = `https://swapi.dev/api/people/?page=${page}`;
+      let res = await axios.get(url);
+      setPeople(res.data.results)
     }
+    getPeopleInfo();
+  }, [page]);
+
+  function next() {
+    setPage(p => p + 1);
   }
 
-  componentDidMount = async () => {
-    let url = `https://swapi.dev/api/people/?page=${this.state.page}`;
-    let res = await axios.get(url);
-    this.setState({
-        people: res.data.results
-    });
+  function previous() {
+    setPage(p => p - 1);
   }
-
-  next = () => {
-    this.setState((prevState) => {
-      return { page: prevState.page + 1 };
-    }, this.componentDidMount);
-  }
-
-  previous = () => {
-    this.setState((prevState) => {
-      return { page: prevState.page - 1 };
-    }, this.componentDidMount);
-  }
-
-  getPrevButton() {
-    return (
-      <button onClick={this.previous}>Previous</button>
-    );
-  }
-
-  getNextButton() {
-    return (
-      <button onClick={this.next}>Next</button>
-    );
-  }
-
-  render () {
-    const people = this.state.people;
-    return (
-      <div className="home">
-        <section className="page-content">
-          {people.map((people, idx) => {
-            return <PersonCard key={idx} people={people} />;
-          })}
-        </section>
-        <div className="pagination">
-          {this.state.page > 1 ? this.getPrevButton() : null}
-          {this.state.page < 9 ? this.getNextButton() : null}
-        </div>
+  return (
+    <div className="home">
+      <section className="page-content">
+        {people.length && people.map((people, idx) => {
+          return (
+            <PersonCard 
+              key={idx} 
+              people={people}
+            />
+          );
+        })}
+      </section>
+      <div className="pagination">
+        {
+          page > 1 
+            ? <button onClick={previous}>Previous</button> 
+            : null
+        }
+        {
+          page < 9 
+            ? <button onClick={next}>Next</button> 
+            : null
+        }
       </div>
-    )
-  }
+    </div>
+  );
+  
 }
 
 export default Home;
-
-
-Home.propTypes = {
-  page: PropTypes.number.isRequired
-};
--- src/PersonDetail/PersonDetail.js
+++ src/PersonDetail/PersonDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './PersonDetail.css';
@@ -9,69 +9,62 @@ function withParams(Component) {
   return (props) => <Component {...props} params={useParams()} />;
 }
 
-class PersonDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      person: {}
+function getHomePlanet(planet) {
+  const planetNames = ["Tatooine", "Alderaan", "Yavin IV", "Hoth", "Dagobah", "Bespin", "Endor", "Naboo","Coruscant", "Kamino", "Geonosis", "Utapau", "Mustafar", "Kashyyyk", "Polis Massa", "Mygeeto", "Felucia", "Cato Neimoidia", "Saleucami", "Stewjon", "Eriadu", "Corellia", "Rodia", "Nal Hutta", "Dantooine", "Bestine IV", "Ord Mantell", "unknown", "Trandosha", "Socorro", "Mon Cala", "Chandrila", "Sullust", "Toydaria", "Malastare", "Dathomir", "Ryloth", "Aleen Minor", "Vulpter", "Troiken", "Tund", "Haruun Kal", "Cerea", "Glee Anselm", "Iridonia", "Tholoth", "Iktotch", "Quermia", "Dorin", "Champala", "Mirial", "Serenno", "Concord Dawn", "Zolan", "Ojom", "Skako", "Muunilinst", "Shili", "Kalee", "Umbara", "Jakku"]
+  const url = planet;
+  let planetNum = url.split('/').reverse()[1];
+  planetNum = planetNum - 1;
+  const finalPlanet = planetNames[planetNum];
+  return (
+    <span>
+      {finalPlanet}
+    </span>
+  );
+};
+
+function getFilmList(films) {
+  const filmNames = ["A New Hope", "The Empire Strikes Back", "Return of the Jedi", "The Phantom Menace", "Attack of the Clones", "Revenge of the Sith", "The Force Awakens", "The Last Jedi", "The Rise of Skywalker"];
+  let finalFilmList = [];
+  films.forEach(film => {
+    let filmNum = film.split('/').reverse()[1];
+    filmNum = filmNum - 1;
+    finalFilmList.push(filmNames[filmNum]);
+  })
+  let componetizedFilmList = finalFilmList.map((finalFilmList, idx) => {
+      return (
+        <span key={idx}>
+          {finalFilmList}{idx < (films.length - 1) && ','} 
+        </span>
+      )
+  })
+  return (
+    <span>
+      {componetizedFilmList}
+    </span>
+  );
+};
+
+function PersonDetail() {
+  const [ person, setPerson ] = useState({});
+  const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld, films } = person;
+
+  const params = useParams();
+  const peopleId = params.peopleId;
+  const backgroundStyles = {
+    backgroundImage: 'url(\'/characters/character-' + peopleId + '.jpg\')',
+  };
+
+  useEffect(function() {
+    async function getPersonInfo() {
+      let url = `https://swapi.dev/api/people/${params.peopleId}/`;
+      let res = await axios.get(url);
+      setPerson(res.data);
     }
-  }
+    getPersonInfo();
+  }, [params]);
 
-  componentDidMount = async () => {
-    const peopleId = this.props.params.peopleId;
-    let url = `https://swapi.dev/api/people/${peopleId}/`
-    let res = await axios.get(url);
-    this.setState({
-        person: res.data
-    });
-  };
-
-  getFilmList(films) {
-    const filmNames = ["A New Hope", "The Empire Strikes Back", "Return of the Jedi", "The Phantom Menace", "Attack of the Clones", "Revenge of the Sith", "The Force Awakens", "The Last Jedi", "The Rise of Skywalker"];
-    let finalFilmList = [];
-    films.forEach(film => {
-      let filmNum = film.split('/').reverse()[1];
-      filmNum = filmNum - 1;
-      finalFilmList.push(filmNames[filmNum]);
-    })
-    let componetizedFilmList = finalFilmList.map((finalFilmList, idx) => {
-        return (
-          <span key={idx}>
-            {finalFilmList}{idx < (films.length - 1) && ','} 
-          </span>
-        )
-    })
-    return (
-      <span>
-        {componetizedFilmList}
-      </span>
-    );
-  };
-
-  getHomePlanet(planet) {
-    const planetNames = ["Tatooine", "Alderaan", "Yavin IV", "Hoth", "Dagobah", "Bespin", "Endor", "Naboo","Coruscant", "Kamino", "Geonosis", "Utapau", "Mustafar", "Kashyyyk", "Polis Massa", "Mygeeto", "Felucia", "Cato Neimoidia", "Saleucami", "Stewjon", "Eriadu", "Corellia", "Rodia", "Nal Hutta", "Dantooine", "Bestine IV", "Ord Mantell", "unknown", "Trandosha", "Socorro", "Mon Cala", "Chandrila", "Sullust", "Toydaria", "Malastare", "Dathomir", "Ryloth", "Aleen Minor", "Vulpter", "Troiken", "Tund", "Haruun Kal", "Cerea", "Glee Anselm", "Iridonia", "Tholoth", "Iktotch", "Quermia", "Dorin", "Champala", "Mirial", "Serenno", "Concord Dawn", "Zolan", "Ojom", "Skako", "Muunilinst", "Shili", "Kalee", "Umbara", "Jakku"]
-    const url = planet;
-    let planetNum = url.split('/').reverse()[1];
-    planetNum = planetNum - 1;
-    const finalPlanet = planetNames[planetNum];
-    return (
-      <span>
-        {finalPlanet}
-      </span>
-    );
-  };
-
-  render() {
-    const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld } = this.state.person;
-    const films = this.state.person.films;
-    //let { peopleId } = useParams();
-    const peopleId = this.props.params.peopleId;
-    //const peopleIdNum = this.props.match.params.peopleId;
-    const backgroundStyles = {
-        backgroundImage: 'url(\'/characters/character-' + peopleId + '.jpg\')',
-    };
-    return (
-      <div className="page-content">
+  return (
+    <div className="page-content">
         <section className="person-detail">
           <div className="content">
             <h1>{name}</h1>
@@ -88,10 +81,10 @@ class PersonDetail extends Component {
                   <dl><dt>Eye color:</dt><dd>{eye_color}</dd></dl>
                   <dl><dt>Birth year:</dt><dd>{birth_year}</dd></dl>
                   <dl><dt>Gender:</dt><dd>{gender}</dd></dl>
-                  <dl><dt>Homeworld:</dt>{homeworld ? this.getHomePlanet(homeworld) : "unknown"}<dd></dd></dl>
+                  <dl><dt>Homeworld:</dt>{homeworld ? getHomePlanet(homeworld) : "unknown"}<dd></dd></dl>
                 </div>
                 <div className="stat--notes">
-                  <p> Appears in: <span className="emph">{films ? this.getFilmList(films) : "none" }</span></p>
+                  <p> Appears in: <span className="emph">{films ? getFilmList(films) : "none" }</span></p>
                 </div>
               </div>
             </aside>
@@ -103,8 +96,7 @@ class PersonDetail extends Component {
           </div>
         </section>
       </div>
-    )
-  }
+  )
 }
 
 PersonDetail.propTypes = {
